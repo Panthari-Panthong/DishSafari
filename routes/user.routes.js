2ae0d66195495331b1c5a4864e3f5ff35debe6f6
Panthari Panthong <panthari.panthong@gmail.com>
2023-04-28 12:31:01 +0200

    Separator textarea Ingredients and directions to an array

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,4 +11,49 @@ router.get("/profile", async (req, res, next) => {
   }
 });
 
+// Create Recipe
+// GET route ==> to display the create form to user
+router.get("/create", (req, res, next) => {
+  res.render("user/createRecipe", { userInSession: req.session.currentUser });
+});
+
+// POST create route ==> to process form data
+router.post("/create", (req, res, next) => {
+  try {
+    // ---------- Start Ingredients -------------------
+    // Separator ingredients by (comma) , to an array
+    // Before adding in DB
+    const ingredients = req.body.ingredients;
+    const splitIngredients = ingredients.split(",");
+
+    // Removes whitespace from both ends of a string
+    const ingredient = splitIngredients.map((element) => {
+      return element.trim();
+    });
+
+    // ---------- Start Ingredients -------------------
+
+    // ---------- Start Directions -------------------
+    //Separator Direction by "Step" word to an array
+    const directions = req.body.directions;
+    const splitDirections = directions.split("Step");
+
+    // Removes whitespace from both ends of a string
+    const removesWhitespace = splitDirections.map((element) => {
+      return element.trim();
+    });
+
+    //Remove empty string in the direction array
+    const direction = removesWhitespace.filter((str) => {
+      return str !== "";
+    });
+    // ---------- End Directions -------------------
+
+    console.log("Name : ", req.session.currentUser.username);
+    // console.log("BODY", req.body);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
