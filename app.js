64d7ace60e8d023a54eb50a0a2b06193afaa3073
Scott Shepherd <scottshep86@gmail.com>
2024-02-26 16:24:11 +0100

    Made some improvements to the Project: added the font we both liked form the beginning, changed the filter by all Recipes including the placing of the recipes on the main side of the page, changed the profile recipes to make them a bit smaller, creating a recipe form has placeholders although I am not 100% happy and added some general CSS

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv").config();
+require('dotenv').config();
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require('./config')(app);
 // use session here:                 V
-require("./config/session.config")(app);
+require('./config/session.config')(app);
 
 // default value for title local
-const capitalize = require("./utils/capitalize");
-const projectName = "DishSafari";
+const capitalize = require('./utils/capitalize');
+const projectName = 'DishSafari';
 
 app.locals.appTitle = `${projectName}`;
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/", indexRoutes);
+const indexRoutes = require('./routes/index.routes');
+app.use('/', indexRoutes);
 
 // Auth route (signup , login, logout)
-const authRoute = require("./routes/auth.routes");
-app.use("/", authRoute);
+const authRoute = require('./routes/auth.routes');
+app.use('/', authRoute);
 
 // User route (profile , createRecipe , editRecipe, deleteRecipe)
-const userRoute = require("./routes/user.routes");
-app.use("/user", userRoute);
+const userRoute = require('./routes/user.routes');
+app.use('/user', userRoute);
 
 // Recipe route (allRecipes, oneRecipe, randomRecipe)
-const recipeRoutes = require("./routes/recipes.routes");
-app.use("/recipes", recipeRoutes);
+const recipeRoutes = require('./routes/recipes.routes');
+app.use('/recipes', recipeRoutes);
 
-const reviewRoute = require("./routes/review.routes");
-app.use("/", reviewRoute);
+const reviewRoute = require('./routes/review.routes');
+app.use('/', reviewRoute);
 
 // BMI
-const bmiRoute = require("./routes/bmi.routes");
-app.use("/bmi", bmiRoute);
+const bmiRoute = require('./routes/bmi.routes');
+app.use('/bmi', bmiRoute);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
